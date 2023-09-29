ffefc89bb6d34159a414c89f3c5f712eba548345
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-09-29 12:31:20 -0500

    got search radius working and starting to fine tune navbar and banner

--- src/components/header/Banner.js
+++ src/components/header/Banner.js
@@ -4,19 +4,42 @@ import { Link, useNavigate } from "react-router-dom";
 export const Banner = () => {
   const navigate = useNavigate();
   return (
-    // <div className="banner">
-    <div class="background-container">
-      <img
-        id="moon-img"
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png"
-        alt=""
-      />
+    <div className="banner">
+      {localStorage.getItem("skies_user") ? (
+        <div className="banner-item" id="banner-logout">
+          <Link
+            className="banner-link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("skies_user");
+              navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
 
-      <div class="stars"></div>
-      <div class="twinkling"></div>
-      <div class="clouds"></div>
+      <div className="banner-item" id="banner-title">
+        Site Name
+      </div>
+      <div className="banner-item" id="banner-logo">
+        *img*
+      </div>
+      <div className="background-container">
+        <img
+          id="moon-img"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png"
+          alt=""
+        />
+
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+      </div>
     </div>
-    // </div>
   );
 };
 
